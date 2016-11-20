12afdc6662b45979b5c7078af080f0b2c93efa59
xin541322049 <541322049@qq.com>
2016-11-20 10:10:24 +0800

    Controller

--- src/main/java/bl/implementation/Member.java
+++ src/main/java/bl/implementation/Member.java
@@ -1,24 +1,21 @@
 package bl.implementation;
 
-import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
 import bl.service.MemberBLService;
 import vo.MemberVO;
 import vo.OrderVO;
-import setup.*;
-
 
 public class Member extends User implements MemberBLService {
 	
 	public Order ordermanager = new Order();
-	public Search searchmanager =new Search();
 	
 	private String name;
-	private Date birthday;
+	private String birthday;
 	private String phone;
 	private MemberVO vo;
+<<<<<<< HEAD
 	private MemberType type;
 	private Enterprise enterprise;
 	private  Credit credit;
@@ -28,20 +25,29 @@ public class Member extends User implements MemberBLService {
 	
 	public Member(String ID,String password,Date birthday,String name,String phone,
 			Double credit,MemberType type,Enterprise enterprise) {
+=======
+	private List<String> creditList;
+	public Member(String ID,String password,String birthday,String name,String phone,double credit) {
+>>>>>>> parent of e8a2e22... 改动
 		setID(ID);
 		setPassword(password);
 		setName(name);
 		this.birthday=birthday;
+<<<<<<< HEAD
 		setPhone(phone);
 		this.credit=new Credit(credit);
 		setType(type);
 		setEnterprise(enterprise);
+=======
+		this.phone=phone;
+		this.credit=credit;
+		//creditList.add(""+credit);
+>>>>>>> parent of e8a2e22... 改动
 	}
 
 	public MemberVO getMInformation (String memberID) {
 		return vo;
 	}
-	
 	public boolean saveMInformation (String memberID, MemberVO M) {
 		if(memberID.equals(ID)){
 			setVo(vo);
@@ -49,12 +55,42 @@ public class Member extends User implements MemberBLService {
 		}
 		return false;
 	}
+<<<<<<< HEAD
 
+=======
+	public List<String> getCreditList (String memberID) {
+		return creditList;
+	}
+	@Override
+	public String getCredit(String memberID) {
+		// TODO Auto-generated method stub
+		return ""+credit;
+	}
+	@Override
+	public boolean updateCredit(String memberID, double newCredit) {
+		// TODO Auto-generated method stub
+		if(memberID.equals(ID)){
+			setCredit(newCredit);
+			//creditList.add(""+credit);
+			return true;
+		}
+		return false;
+	}
+	@Override
+	public boolean checkCredit(String memberID) {
+		// TODO Auto-generated method stub
+		if(credit>=0){
+			return true;
+		}
+		return false;
+	}
+	@Override
+>>>>>>> parent of e8a2e22... 改动
 	public OrderVO getOrder(String orderID) {
 		// TODO Auto-generated method stub
 		return ordermanager.getOrder(orderID);
 	}
-
+	@Override
 	public List<OrderVO> gerOrderList(String userID) {
 		// TODO Auto-generated method stub
 		if(ID.equals(userID)){
@@ -63,22 +99,22 @@ public class Member extends User implements MemberBLService {
 			return null;
 		}
 	}
-
+	@Override
 	public void cancelOrder(String orderID) {
 		// TODO Auto-generated method stub
 		
 	}
-
+	@Override
 	public ArrayList<OrderVO> getUnOrder(ArrayList<OrderVO> orderList) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	@Override
 	public ArrayList<OrderVO> getAlOrder(ArrayList<OrderVO> orderList) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	@Override
 	public ArrayList<OrderVO> getAandCOrder(ArrayList<OrderVO> orderList) {
 		// TODO Auto-generated method stub
 		return null;
@@ -87,34 +123,36 @@ public class Member extends User implements MemberBLService {
 	public String getPassword() {
 		return passWord;
 	}
-	
 	public void setPassword(String password) {
 		this.passWord = password;
 	}
-	
 	public String getName() {
 		return name;
 	}
-	
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	public Date getBirthday() {
+	public String getBirthday() {
 		return birthday;
 	}
-	
-	public void setBirthday(Date birthday) {
+	public void setBirthday(String birthday) {
 		this.birthday = birthday;
 	}
-	
 	public String getPhone() {
 		return phone;
 	}
-	
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+<<<<<<< HEAD
+=======
+	public double getCredit() {
+		return credit;
+	}
+	public void setCredit(double credit) {
+		this.credit = credit;
+	}
+>>>>>>> parent of e8a2e22... 改动
 
 	public String getID() {
 		return ID;
@@ -136,20 +174,5 @@ public class Member extends User implements MemberBLService {
 //		return creditList;
 //	}
 
-	public MemberType getType() {
-		return type;
-	}
-
-	public void setType(MemberType type) {
-		this.type = type;
-	}
-
-	public Enterprise getEnterprise() {
-		return enterprise;
-	}
-
-	public void setEnterprise(Enterprise enterprise) {
-		this.enterprise = enterprise;
-	}
-
+	
 }
--- src/main/java/bl/stub/MemberBLStub.java
+++ src/main/java/bl/stub/MemberBLStub.java
@@ -27,64 +27,84 @@ public class MemberBLStub implements MemberBLService {
 		this.birth = birth;
 	}
 	
+	@Override
 	public MemberVO getMInformation(String memberID) {
 		// TODO Auto-generated method stub
 		return new MemberVO(memberID,pass, name, birth, phone, credit);
 	}
 
+	@Override
 	public boolean saveMInformation(String memberID, MemberVO M) {
 		// TODO Auto-generated method stub
 		System.out.println("用户信息保存成功");
 		return true;
 	}
 
+<<<<<<< HEAD
 	public ArrayList<Double> getCreditList(String memberID) {
+=======
+	@Override
+	public List<String> getCreditList(String memberID) {
+>>>>>>> parent of e8a2e22... 改动
 		// TODO Auto-generated method stub
 		ArrayList<Double> creditList = new ArrayList<Double>();
 		creditList.add(100.0);
 		return creditList;
 	}
 
+<<<<<<< HEAD
 	public Double getCredit(String memberID) {
+=======
+	@Override
+	public String getCredit(String memberID) {
+>>>>>>> parent of e8a2e22... 改动
 		// TODO Auto-generated method stub
 		return 0.0;
 	}
 
+	@Override
 	public boolean updateCredit(String memberID, double newCredit) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	@Override
 	public boolean checkCredit(String memberID) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	@Override
 	public OrderVO getOrder(String orderID) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Override
 	public List<OrderVO> gerOrderList(String userID) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Override
 	public void cancelOrder(String orderID) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	@Override
 	public List<OrderVO> getUnOrder(ArrayList<OrderVO> orderList) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Override
 	public List<OrderVO> getAlOrder(ArrayList<OrderVO> orderList) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Override
 	public List<OrderVO> getAandCOrder(ArrayList<OrderVO> orderList) {
 		// TODO Auto-generated method stub
 		return null;
