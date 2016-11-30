ced5d01666bc6651ed34a9309c632ae14257df82
素水 <324716891@qq.com>
2016-11-30 20:45:46 +0800

    更新所有枚举类

--- src/main/java/other/PromotionType.java
+++ src/main/java/other/PromotionType.java
@@ -3,9 +3,20 @@ package other;
 /**
  * Created by CROFF on 2016/11/29.
  * 促销策略的类型，分为折扣和满减
- * Discount：折扣
- * Reduce：满减
+ * Discount：折扣(0)
+ * Reduce：满减(1)
+ * @author CROFF
+ * @version 2016-11-30
  */
 public enum PromotionType {
-	Discount, Reduce;
+	Discount(0), Reduce(1);
+	
+	int value;
+	private PromotionType(int value) {
+		this.value = value;
+	}
+	
+	public int getValue() {
+		return value;
+	}
 }
