0e45f0e9343713447f85a1b6fc652647a27bd0b9
素水 <324716891@qq.com>
2016-11-30 23:17:16 +0800

    更新bl和ui之间少量接口
    去除一些重复的接口，分工更明确

--- src/main/java/bl/service/PromotionBLService.java
+++ src/main/java/bl/service/PromotionBLService.java
@@ -15,5 +15,6 @@ public interface PromotionBLService {
 	public boolean addPromotion(PromotionVO promotion);	//添加营销策略
 	public boolean deletePromotion(String promotionID);	//删除营销策略
 	public boolean updatePromotion(String promotionID, PromotionVO promotion);	//更新营销策略信息
-	public ArrayList<PromotionVO> getPromotionList();	//获得营销策略列表
+	public ArrayList<PromotionVO> getHotelPromotionList(String hotelID);	//获得酒店营销策略列表
+	public ArrayList<PromotionVO> getAllPromotionList();	//获得网站营销策略列表
 }
