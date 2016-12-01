783329f11d064edf9cfa493c777e4db0655eda74
xyj15 <591024170@qq.com>
2016-12-01 16:11:26 +0800

    Saler界面转换完成

--- src/main/java/ui/presentation/SalerAddVIPUI.java
+++ src/main/java/ui/presentation/SalerAddVIPUI.java
@@ -13,9 +13,9 @@ import ui.controller.PromotionController;
 public class SalerAddVIPUI extends Application {
     @Override
     public void start(Stage primaryStage)throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("SalerAddVIPDiscount.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("SalerAddVIP.fxml"));
         PromotionController.setPrimaryStage(primaryStage);
-        Scene myScene = new Scene(root,960,540);
+        Scene myScene = new Scene(root,318,538);
         primaryStage.setScene(myScene);
         primaryStage.show();
     }
