ee1ab1199ac129a9fe5895c04e9f1a802f2ab61f
素水 <324716891@qq.com>
2016-12-02 19:22:40 +0800

    Order的VO、PO更新
    添加变量保存撤销订单时间

--- src/main/java/bl/implementation/Order.java
+++ src/main/java/bl/implementation/Order.java
@@ -2,10 +2,12 @@ package bl.implementation;
 
 import bl.service.OrderBLService;
 import data.service.OrderDataService;
+import other.OrderStatus;
 import po.OrderPO;
 import vo.OrderVO;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Order模块bl的实现类
@@ -14,43 +16,63 @@ import java.util.ArrayList;
  */
 public class Order implements OrderBLService {
 	
-	private ArrayList<OrderPO> orderList;
+	private ArrayList<OrderVO> orderList;
 	private OrderDataService orderDataService;
 	private String userID;
 	
 	public Order(String userID) {
 		this.userID = userID;
-		orderList = orderDataService.getOrderList(userID);
+		updateDataFromFile();
 	}
 	
 	@Override
 	public ArrayList<OrderVO> getOrderList() {
-		return null;
+		updateDataFromFile();
+		return orderList;
 	}
 	
 	@Override
 	public ArrayList<OrderVO> getExcutedOrders() {
-		return null;
+		updateDataFromFile();
+		return filterList(orderList, OrderStatus.Executed);
 	}
 	
 	@Override
 	public ArrayList<OrderVO> getUnexcutedOrders() {
-		return null;
+		updateDataFromFile();
+		return filterList(orderList, OrderStatus.Unexecuted);
 	}
 	
 	@Override
 	public ArrayList<OrderVO> getAbnormalOrders() {
-		return null;
+		updateDataFromFile();
+		return filterList(orderList, OrderStatus.Abnormal);
 	}
 	
 	@Override
 	public ArrayList<OrderVO> getCanceledOrders() {
-		return null;
+		updateDataFromFile();
+		return filterList(orderList, OrderStatus.Canceled);
 	}
 	
 	@Override
 	public boolean cancelOrder(String orderID) {
-		return false;
+		updateDataFromFile();
+		OrderVO orderVO;
+		boolean foundOrder = false;
+		for(int i=0; i<orderList.size(); i++) {
+			orderVO = orderList.get(i);
+			if(orderVO.getOrderID().equals(orderID)) {
+				foundOrder = true;
+				break;
+			}
+		}
+		if(!foundOrder) {
+			return false;
+		} else {
+			
+			return true;
+		}
 	}
 	
 	@Override
@@ -62,41 +84,51 @@ public class Order implements OrderBLService {
 	public boolean cancelAbnormalOrder(String orderID, double recover) {
 		return false;
 	}
+	
+	public void updateDataFromFile() {
+		ArrayList<OrderPO> orderPOArrayList = orderDataService.getOrderList(userID);
+		orderList = new ArrayList<OrderVO>();
+		OrderPO orderPO;
+		OrderVO orderVO;
+		for (int i = 0; i < orderList.size(); i++) {
+			orderPO = orderPOArrayList.get(i);
+			String memberID = orderPO.getMemberID();
+			String hotelID = orderPO.getHotelID();
+			String orderID = orderPO.getOrderID();
+			OrderStatus orderStatus = orderPO.getOrderStatus();
+			Date createTime = orderPO.getCreateTime();
+			Date checkinTime = orderPO.getCheckinTime();
+			Date actualCheckinTime = orderPO.getActualCheckinTime();
+			Date latestCheckinTime = orderPO.getLatestCheckinTime();
+			Date checkoutTime = orderPO.getCheckoutTime();
+			Date actualCheckoutTime = orderPO.getActualCheckoutTime();
+			int numberOfRoom = orderPO.getNumberOfRoom();
+			String roomName = orderPO.getRoomName();
+			int numberOfClient = orderPO.getNumberOfClient();
+			boolean haveKids = orderPO.getHaveKids();
+			double score = orderPO.getScore();
+			String evaluation = orderPO.getEvaluation();
+			double recover = orderPO.getRecover();
+			String promotionID = orderPO.getPromotionID();
+			double price = orderPO.getPrice();
+			Date cancelTime = orderPO.getCancelTime();
+			orderVO = new OrderVO(memberID, hotelID, orderID, orderStatus, createTime,
+					checkinTime, actualCheckinTime, latestCheckinTime, checkoutTime,
+					actualCheckoutTime, numberOfRoom, roomName, numberOfClient, haveKids,
+					score, evaluation, recover, promotionID, price, cancelTime);
+			orderList.add(orderVO);
+		}
+	}
+	
+	public ArrayList<OrderVO> filterList(ArrayList<OrderVO> orderList, OrderStatus orderStatus) {
+		ArrayList<OrderVO> orderVOArrayList = new ArrayList<OrderVO>();
+		OrderVO orderVO;
+		for(int i=0; i<orderList.size(); i++) {
+			orderVO = orderList.get(i);
+			if(orderVO.getOrderStatus().equals(orderStatus)) {
+				orderVOArrayList.add(orderVO);
+			}
+		}
+		return orderVOArrayList;
+	}
 }
-
-//	@Override
-//	public ArrayList<OrderVO> getOrderList() {
-//		orderList = orderDataService.getOrderList(userID);
-//
-//		ArrayList<OrderVO> orderVOList = new ArrayList<OrderVO>();
-//		OrderPO orderPO;
-//		OrderVO orderVO;
-//		for (int i = 0; i < orderList.size(); i++) {
-//			orderPO = orderList.get(i);
-//			String memberID = orderPO.getMemberID();
-//			String hotelID = orderPO.getHotelID();
-//			String orderID = orderPO.getOrderID();
-//			OrderStatus orderStatus = orderPO.getOrderStatus();
-//			Date createTime = orderPO.getCreateTime();
-//			Date checkinTime = orderPO.getCheckinTime();
-//			Date actualCheckinTime = orderPO.getActualCheckinTime();
-//			Date latestCheckinTime = orderPO.getLatestCheckinTime();
-//			Date checkoutTime = orderPO.getCheckoutTime();
-//			Date actualCheckoutTime = orderPO.getActualCheckoutTime();
-//			int numberOfRoom = orderPO.getNumberOfRoom();
-//			String roomName = orderPO.getRoomName();
-//			int numberOfClient = orderPO.getNumberOfClient();
-//			boolean haveKids = orderPO.getHaveKids();
-//			double score = orderPO.getScore();
-//			String evaluation = orderPO.getEvaluation();
-//			double recover = orderPO.getRecover();
-//			String promotionID = orderPO.getPromotionID();
-//			double price = orderPO.getPrice();
-//			orderVO = new OrderVO(memberID, hotelID, orderID, orderStatus, createTime,
-//					checkinTime, actualCheckinTime, latestCheckinTime, checkoutTime,
-//					actualCheckoutTime, numberOfRoom, roomName, numberOfClient, haveKids,
-//					score, evaluation, recover, promotionID, price);
-//			orderVOList.add(orderVO);
-//		}
-//		return orderVOList;
-//	}
