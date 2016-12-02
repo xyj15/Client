1cb7fdf54fb1908df59aa0b655d0bd52d03f3a40
素水 <324716891@qq.com>
2016-12-02 11:37:27 +0800

    Login添加注释

--- src/main/java/bl/implementation/Login.java
+++ src/main/java/bl/implementation/Login.java
@@ -16,7 +16,7 @@ import java.util.Date;
 /**
  * login模块bl的实现类
  * @author CROFF
- * @version 2016-11-28
+ * @version 2016-12-2
  */
 public class Login implements LoginBLService {
 	
@@ -25,15 +25,27 @@ public class Login implements LoginBLService {
 	private HotelDataService hotelDataService;
 	private ManagerDataService managerDataService;
 	
+	/**
+	 * 空构造方法
+	 */
 	public Login() {
 		
 	}
 	
+	/**
+	 * 检查网络情况
+	 * @return 可以连接到网络则true，否则false
+	 */
 	@Override
 	public boolean checkNetwork() {
 		return true;
 	}
 	
+	/**
+	 * 检查用户ID是否存在
+	 * @param userID 用户ID
+	 * @return 存在则true，否则false
+	 */
 	@Override
 	public boolean existUserID(String userID) {
 		User user = getUser(userID);
@@ -44,6 +56,11 @@ public class Login implements LoginBLService {
 		}
 	}
 	
+	/**
+	 * 检查密码是否合法可用
+	 * @param password 密码
+	 * @return 合法则true，否则false
+	 */
 	@Override
 	public boolean validPassword(String password) {
 		int lengthOfPassword = password.length();
@@ -63,6 +80,12 @@ public class Login implements LoginBLService {
 		return true;
 	}
 	
+	/**
+	 * 用户登录
+	 * @param userID 用户ID
+	 * @param password 密码
+	 * @return 登录成功则true，密码错误则false
+	 */
 	@Override
 	public boolean login(String userID, String password) {
 		User user = getUser(userID);
@@ -74,6 +97,11 @@ public class Login implements LoginBLService {
 		}
 	}
 	
+	/**
+	 * 注册客户
+	 * @param memberVO 新注册客户的信息
+	 * @return 注册成功则true，否则false
+	 */
 	@Override
 	public boolean register(MemberVO memberVO) {
 		String userID = memberVO.getUserID();
@@ -90,6 +118,11 @@ public class Login implements LoginBLService {
 		return memberDataService.addMember(memberPO);
 	}
 	
+	/**
+	 * 获得用户类型
+	 * @param userID 用户ID
+	 * @return 用户类型：Member、Hotel、Saler、Manager
+	 */
 	@Override
 	public UserType getUserType(String userID) {
 		int lengthOfUserID = userID.length();
@@ -106,6 +139,11 @@ public class Login implements LoginBLService {
 		return null;
 	}
 	
+	/**
+	 * 根据userID获取User的信息
+	 * @param userID
+	 * @return user的信息
+	 */
 	private User getUser(String userID) {
 		User user = null;
 		UserType userType = getUserType(userID);
@@ -126,6 +164,11 @@ public class Login implements LoginBLService {
 		return user;
 	}
 	
+	/**
+	 * 判断一个char字符是否是数字或字母
+	 * @param c 字符
+	 * @return true or false
+	 */
 	private boolean isCharAndNum(char c) {
 		if((c>='0'&&c<='9') || (c>='a'&&c<='z') || (c>='A'&&c<='Z')) {
 			return true;
