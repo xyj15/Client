35224d73553a9eeeb51365751b218660f0a9b396
971472013 <971472013@qq.com>
2016-12-07 20:13:27 +0800

    x

---
+++ src/main/java/other/TableDateForMemberHisHotel.java
@@ -0,0 +1,18 @@
+package other;
+
+import javafx.beans.property.SimpleStringProperty;
+
+/**
+ * Created by 97147 on 2016/12/7.
+ */
+public class TableDateForMemberHisHotel {
+    private final SimpleStringProperty num = new SimpleStringProperty();
+    private final SimpleStringProperty date = new SimpleStringProperty();
+    private final SimpleStringProperty action = new SimpleStringProperty();
+    private final SimpleStringProperty change = new SimpleStringProperty();
+    private final SimpleStringProperty now = new SimpleStringProperty();
+
+    TableDateForMemberHisHotel(){
+
+    }
+}
