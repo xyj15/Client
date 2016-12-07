1d4f043289287ed337f7027f07719eed8854f240
素水 <324716891@qq.com>
2016-12-07 20:06:40 +0800

    更新member接口

--- src/main/java/bl/stub/MemberBLStub.java
+++ src/main/java/bl/stub/MemberBLStub.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import bl.service.MemberBLService;
 import other.MemberType;
 import other.OrderAction;
+import other.OrderStatus;
 import vo.CreditChangeVO;
 import vo.MemberVO;
 import vo.OrderVO;
@@ -13,6 +14,7 @@ import vo.OrderVO;
 public class MemberBLStub implements MemberBLService {
 	
 	private MemberVO memberVO;
+	private ArrayList<OrderVO> orderList;
 	private double credit = 100;
 	
 	public MemberBLStub() {
@@ -27,6 +29,32 @@ public class MemberBLStub implements MemberBLService {
 		String enterprise = "南京大学有限公司";
 		memberVO = new MemberVO(memberID, password, name, tel, level, discount,
 				memberType, birthday, enterprise);
+		
+		memberID = "12345678";
+		String hotelID = "123456";
+		String orderID = "00000";
+		OrderStatus orderStatus = OrderStatus.Executed;
+		Date createTime = new Date();
+		Date checkinTime = new Date();
+		Date actualCheckinTime = new Date();
+		Date latestCheckinTime = new Date();
+		Date checkoutTime = new Date();
+		Date actualCheckoutTime = new Date();
+		int numberOfRoom = 4;
+		String roomName = "商务大床房";
+		int numberOfClient = 4;
+		boolean haveKids = false;
+		double score = 5;
+		String evaluation = "床太硬了不酥服555555";
+		double recover = 0.5;
+		String promotionID = "00000";
+		double price = 99999;
+		Date cancelTime = new Date();
+		OrderVO orderVO = new OrderVO(memberID, hotelID, orderID, orderStatus, createTime,
+				checkinTime, actualCheckinTime, latestCheckinTime, checkoutTime, actualCheckoutTime,
+				numberOfRoom, roomName, numberOfClient, haveKids, score, evaluation, recover,
+				promotionID, price, cancelTime);
+		orderList.add(orderVO);
 	}
 	
 	@Override
@@ -86,6 +114,52 @@ public class MemberBLStub implements MemberBLService {
 	public String getEnterprise() {
 		System.out.println("获取合作企业名称成功");
 		return memberVO.getEnterprise();
+	}
+	
+	@Override
+	public ArrayList<OrderVO> getOrderList() {
+		System.out.println("获取订单列表成功");
+		return orderList;
+	}
+	
+	@Override
+	public ArrayList<OrderVO> getExcutedOrders() {
+		System.out.println("获取已执行订单列表成功");
+		orderList.get(0).setOrderStatus(OrderStatus.Executed);
+		return orderList;
+	}
+	
+	@Override
+	public ArrayList<OrderVO> getUnexcutedOrders() {
+		System.out.println("获取未执行订单列表成功");
+		orderList.get(0).setOrderStatus(OrderStatus.Unexecuted);
+		return orderList;
+	}
+	
+	@Override
+	public ArrayList<OrderVO> getAbnormalOrders() {
+		System.out.println("获取异常订单列表成功");
+		orderList.get(0).setOrderStatus(OrderStatus.Abnormal);
+		return orderList;
+	}
+	
+	@Override
+	public ArrayList<OrderVO> getCanceledOrders() {
+		System.out.println("获取已撤销订单列表成功");
+		orderList.get(0).setOrderStatus(OrderStatus.Canceled);
+		return orderList;
+	}
+	
+	@Override
+	public boolean cancelOrder(String orderID) {
+		System.out.println("撤销订单成功");
+		return true;
+	}
+	
+	@Override
+	public boolean evaluateOrder(String orderID, double score, String comment) {
+		System.out.println("评价订单成功");
+		return true;
 	}
 	
 	@Override
