667d24dbd59412134c5a607726e4042c637e39ae
971472013 <971472013@qq.com>
2016-12-11 20:32:11 +0800

    debug
    debug for date

--- src/main/java/ui/presentation/HotelDelayOrderUI.java
+++ src/main/java/ui/presentation/HotelDelayOrderUI.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import ui.controller.HotelController;
 import ui.controller.LoginController;
 
 /**
@@ -15,7 +16,8 @@ public class HotelDelayOrderUI extends Application {
     public void start (Stage primaryStage) throws Exception {
         primaryStage.setTitle("食宿");
         Parent root = FXMLLoader.load(getClass().getResource("HotelDelayOrder.fxml"));
-        LoginController.setPrimaryStage(primaryStage);
+        HotelController.setMinprimaryStage(primaryStage);
+        HotelController.setMinroot(root);
         Scene myScene = new Scene(root,318,538);
         primaryStage.setResizable(false);
         primaryStage.setScene(myScene);
