d5196a0e31bcbbaa77013fa66fa48168328b3707
素水 <324716891@qq.com>
2016-12-18 16:49:39 +0800

    RMI

--- src/main/java/data/service/CreditDataService.java
+++ src/main/java/data/service/CreditDataService.java
@@ -2,7 +2,6 @@ package data.service;
 
 import po.CreditChangePO;
 
-import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 
@@ -14,8 +13,8 @@ import java.util.ArrayList;
  */
 public interface CreditDataService extends java.rmi.Remote {
 	
+	public boolean addCreditChange(String memberID, CreditChangePO creditChange) throws RemoteException;	//为客户添加新的信用变化情况
 	public double getCredit(String memberID) throws RemoteException;	//根据客户ID获取客户信用
 	public ArrayList<CreditChangePO> getCreditChange(String memberID) throws RemoteException;	//根据客户ID获取客户信用变化情况列表
 	public boolean setCredit(String memberID, double credit) throws RemoteException;	//设置客户的信用
-	public boolean addCreditChange(String memberID, CreditChangePO creditChange) throws RemoteException;	//为客户添加新的信用变化情况
 }
